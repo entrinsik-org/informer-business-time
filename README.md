d82819af2bde8db4ab56b1854cd44e639810591c
amoro99 <andrew@entrinsik.com>
2018-07-20 13:54:02 -0400

    tests, docs, and code

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # informer-business-time
-Add business time functions to moment.js
+Add business time functions to moment.js. They are all revisions of the functions in [moment-business-time](https://www.npmjs.com/package/moment-business-time). They use the locales of the moment rather than the globally set locale for computation.
+
+### Configuration
+
+Specify locales for differing worktime and holiday sets
+
+in config.json:
+
+    "plugins" : {
+        "@entrinsik/informer-business-time" : {
+          "locales" : [
+            {
+              "name":"en",
+              "workinghours" : {
+                "0": null,
+                "1": ["00:00:00", "23:59:59"],
+                "2": ["00:00:00", "23:59:59"],
+                "3": ["00:00:00", "23:59:59"],
+                "4": ["00:00:00", "23:59:59"],
+                "5": ["00:00:00", "23:59:59"],
+                "6": null
+              },
+              "holidays": [
+                "*-05-31",
+                "2018-10-31"
+              ]
+            }
+          ]
+        }
+     }
+
+worktimes:
+* null means no hours that day
+* otherwise an array with begin and end times, expressed in ISO format
+
+holidays:
+* "*-05-31" means every year, 5/31 is a holiday
+* the upper bound of the worktimes can not be "24:00:00". The maximum is "23:59:59"
+
+To inherit a locales settings, for instance "en", include
+
+"parentLocale" : "en"
+
+        {
+          "name" : "raleigh",
+          "parentLocale" : "en",
+          "workinghours": {
+            "0": null,
+            "1": ["00:00:00","23:59:59"],
+            "2": ["00:00:00","23:59:59"],
+            "3": null,
+            "4": ["00:00:00","23:59:59"],
+            "5": ["00:00:00","23:59:59"],
+            "6": null
+          }
+        }
+
+### Usage
+
+All the functions in the <i>moment-business-time</i> npm package are exposed with the function name ending with "Local":
+<pre>
+isWorkingDayLocal
+isWorkingTimeLocal
+nextWorkingDayLocal
+nextWorkingTimeLocal
+lastWorkingDayLocal
+lastWorkingTimeLocal
+addWorkingTimeLocal
+subtractWorkingTimeLocal
+workingDiffLocal
+</pre>
+
+If you want workinghours and holidays other than the default, set the locale on the moment first:
+
+<pre>
+const start = moment($record.start);
+start.setLocale('raleigh');
+</pre>
+
+caling the above *Local functions will preserve the global locale and use the one set on the moment.
--- test/module-spec.js
+++ test/module-spec.js
@@ -1,10 +1,254 @@
 'use strict';
 
-var should = require('chai').should();
-var lib = require('../');
+const should = require('chai').should();
+const config = {
+    locales: [
+        {
+            name: 'en',
+            workinghours: {
+                0: null,
+                1: ['00:00:00', '23:59:59'],
+                2: ['00:00:00', '23:59:59'],
+                3: ['00:00:00', '13:59:59'],
+                4: ['00:00:00', '23:59:59'],
+                5: ['00:00:00', '23:59:59'],
+                6: null
+            },
+            holidays: [
+                '*-05-31',
+                '2018-10-31'
+            ]
+        },
+        {
+            name: 'raleigh',
+            parentLocale: 'en',
+            workinghours: {
+                0: null,
+                1: [
+                    '00:00:00',
+                    '23:59:59'
+                ],
+                2: [
+                    '00:00:00',
+                    '23:59:59'
+                ],
+                3: null,
+                4: [
+                    '05:00:00',
+                    '13:59:59'
+                ],
+                5: [
+                    '00:00:00',
+                    '23:59:59'
+                ],
+                6: null
+            }
+        }
+    ]
+}
 
-describe('your module', function () {
-    it('should exist', function () {
-        should.exist(lib);
+
+describe('informer-business-time', function () {
+    before(function() {
+        require('../index').register(null,config,()=>{});
     });
+    describe('workingDiffLocal', () => {
+        it('should work for default locale', function() {
+            const moment = require('moment');
+            const start = moment('2018-07-08T20:00');
+            const end = moment('2018-07-14T20:00');
+            end.workingDiffLocal(start, 'days', true).should.equal(5);
+        });
+        it('should work for another locale', () => {
+            const moment = require('moment');
+            const start = moment('2018-07-08T20:00').locale('raleigh');
+            const end = moment('2018-07-14T20:00').locale('raleigh');
+            end.workingDiffLocal(start, 'days', true).should.equal(4);
+            moment.locale().should.equal('en');
+        });
+        it('should not hang on an invalid comparator', () => {
+            const moment = require('moment');
+            const start = [moment('2018-07-08T20:00')];
+            const end = moment('2018-07-14T20:00');
+            should.not.exist(end.workingDiffLocal(start, 'days', true));
+            moment.locale().should.equal('en');
+        });
+        it('should not hang on a null comparator', () => {
+            const moment = require('moment');
+            const start = null;
+            const end = moment('2018-07-14T20:00').locale('raleigh');
+            should.not.exist(end.workingDiffLocal(start, 'days', true));
+            moment.locale().should.equal('en');
+        });
+    });
+    describe('isWorkingDayLocal', () => {
+        it('should work for default locale', function() {
+            const moment = require('moment');
+            const sun = moment('2018-07-08T20:00');
+            const wed = moment('2018-07-11T20:00');
+            sun.isWorkingDayLocal().should.be.false;
+            wed.isWorkingDayLocal().should.be.true;
+        });
+        it('should work for another locale', () => {
+            const moment = require('moment');
+            const sun = moment('2018-07-08T20:00');
+            const wed = moment('2018-07-11T20:00');
+            sun.locale('raleigh');
+            wed.locale('raleigh');
+            sun.isWorkingDayLocal().should.be.false;
+            wed.isWorkingDayLocal().should.be.false;
+        });
+        it('should not hang on an invalid locale', () => {
+            const moment = require('moment');
+            const sun = moment('2018-07-08T20:00');
+            const wed = moment('2018-07-11T20:00');
+            sun.locale('detroit');
+            wed.locale('detroit');
+            sun.isWorkingDayLocal().should.be.false;
+            wed.isWorkingDayLocal().should.be.true;
+        });
+        it('should not hang on a null locale', () => {
+            const moment = require('moment');
+            const sun = moment('2018-07-08T20:00');
+            const wed = moment('2018-07-11T20:00');
+            sun.locale(null);
+            wed.locale(null);
+            sun.isWorkingDayLocal().should.be.false;
+            wed.isWorkingDayLocal().should.be.true;
+        });
+    });
+    describe('isWorkingTimeLocal', () => {
+        it('should work for default locale', function() {
+            const moment = require('moment');
+            const sun = moment('2018-07-08T20:00');
+            const mon = moment('2018-07-09T20:00');
+            const wed = moment('2018-07-11T20:00');
+            sun.isWorkingTimeLocal().should.be.false;
+            wed.isWorkingTimeLocal().should.be.false;
+            mon.isWorkingTimeLocal().should.be.true;
+        });
+        it('should work for another locale', () => {
+            const moment = require('moment');
+            const sun = moment('2018-07-08T20:00');
+            const mon = moment('2018-07-09T20:00');
+            const wed = moment('2018-07-11T20:00');
+            sun.locale('raleigh');
+            wed.locale('raleigh');
+            sun.isWorkingTimeLocal().should.be.false;
+            wed.isWorkingTimeLocal().should.be.false;
+            mon.isWorkingTimeLocal().should.be.true;
+        });
+        it('should not hang on an invalid locale', () => {
+            const moment = require('moment');
+            const sun = moment('2018-07-08T20:00');
+            const mon = moment('2018-07-09T20:00');
+            const wed = moment('2018-07-11T20:00');
+            sun.locale('detroit');
+            wed.locale('detroit');
+            sun.isWorkingTimeLocal().should.be.false;
+            wed.isWorkingTimeLocal().should.be.false;
+            mon.isWorkingTimeLocal().should.be.true;
+        });
+        it('should not hang on a null locale', () => {
+            const moment = require('moment');
+            const sun = moment('2018-07-08T20:00');
+            const mon = moment('2018-07-09T20:00');
+            const wed = moment('2018-07-11T20:00');
+            sun.locale(null);
+            wed.locale(null);
+            sun.isWorkingTimeLocal().should.be.false;
+            wed.isWorkingTimeLocal().should.be.false;
+            mon.isWorkingTimeLocal().should.be.true;
+        });
+    });
+    describe('nextWorkingDayLocal', () => {
+       it('should work for the default locale', () => {
+           const moment = require('moment');
+           const sun = moment('2018-07-08T20:00');
+           const mon = moment('2018-07-09T20:00');
+           (sun.nextWorkingDayLocal().isSame(mon)).should.be.true;
+       });
+        it('should work for another locale', () => {
+            const moment = require('moment');
+            const tue = moment('2018-07-10T20:00').locale('raleigh');
+            const wed = moment('2018-07-11T20:00').locale('raleigh');
+            const thu = moment('2018-07-12T20:00').locale('raleigh');
+            (tue.nextWorkingDayLocal().isSame(wed)).should.be.false;
+            (tue.nextWorkingDayLocal().isSame(thu)).should.be.true;
+        });
+    });
+    describe('nextWorkingTimeLocal', () => {
+        it('should work for the default locale', () => {
+            const moment = require('moment');
+            const sun = moment('2018-07-08T20:00');
+            const mon = moment('2018-07-09T00:00');
+            (sun.nextWorkingTimeLocal().isSame(mon)).should.be.true;
+        });
+        it('should work for another locale', () => {
+            const moment = require('moment');
+            const wed = moment('2018-07-11T00:00').locale('raleigh');
+            const thu = moment('2018-07-12T05:00').locale('raleigh');
+            (wed.nextWorkingTimeLocal().isSame(thu)).should.be.true;
+        });
+    });
+    describe('lastWorkingDayLocal', () => {
+        it('should work for the default locale', () => {
+            const moment = require('moment');
+            const fri = moment('2018-07-06T20:00');
+            const mon = moment('2018-07-09T20:00');
+            (mon.lastWorkingDayLocal().isSame(fri)).should.be.true;
+        });
+        it('should work for another locale', () => {
+            const moment = require('moment');
+            const tue = moment('2018-07-10T20:00').locale('raleigh');
+            const wed = moment('2018-07-11T20:00').locale('raleigh');
+            const thu = moment('2018-07-12T20:00').locale('raleigh');
+            (thu.lastWorkingDayLocal().isSame(wed)).should.be.false;
+            (thu.lastWorkingDayLocal().isSame(tue)).should.be.true;
+        });
+    });
+    describe('lastWorkingTimeLocal', () => {
+        it('should work for the default locale', () => {
+            const moment = require('moment');
+            const sun = moment('2018-07-08T20:00');
+            const fri = moment('2018-07-06T23:59:59');
+            (sun.lastWorkingTimeLocal().isSame(fri)).should.be.true;
+        });
+        it('should work for another locale', () => {
+            const moment = require('moment');
+            const thu = moment('2018-07-12T00:00').locale('raleigh');
+            const tue = moment('2018-07-10T23:59:59').locale('raleigh');
+            (thu.lastWorkingTimeLocal().isSame(tue)).should.be.true;
+        });
+    });
+    describe('addWorkingTimeLocal', () => {
+        it('should work for days', () => {
+            const moment = require('moment');
+            const tue = moment('2018-07-10T20:00').locale('raleigh');
+            const fri = moment('2018-07-13T20:00').locale('raleigh');
+            (tue.addWorkingTimeLocal(2,'day').isSame(fri)).should.be.true;
+        });
+        it('should work for hours', () => {
+            const moment = require('moment');
+            const tue = moment('2018-07-10T20:00').locale('raleigh');
+            const thu = moment('2018-07-12T07:00:01').locale('raleigh');
+            (tue.addWorkingTimeLocal(6, 'hours').isSame(thu,'minute')).should.be.true;
+        });
+    });
+    describe('subtractWorkingTimeLocal', () => {
+        it('should work for days', () => {
+            const moment = require('moment');
+            const tue = moment('2018-07-10T20:00').locale('raleigh');
+            const fri = moment('2018-07-13T20:00').locale('raleigh');
+            (fri.subtractWorkingTimeLocal(2,'day').isSame(tue)).should.be.true;
+        });
+        it('should work for hours', () => {
+            const moment = require('moment');
+            const tue = moment('2018-07-10T20:00').locale('raleigh');
+            const thu = moment('2018-07-12T07:00:01').locale('raleigh');
+            (thu.subtractWorkingTimeLocal(6, 'hours').isSame(tue,'minute')).should.be.true;
+        });
+    });
+
+
 });
